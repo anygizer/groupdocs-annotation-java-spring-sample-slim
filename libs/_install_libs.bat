8f6037572cae03d940bf166319fab00a6aef42f8
aleksey.permyakov <liosha.89@gmail.com>
2014-07-15 10:07:00 +0300

    ANNOTATIONJAVA-189: Update Spring Slim sample for GroupDocs.Annotation for Java to use latest annotation, viewer and aspose libraries

--- libs/_install_libs.bat
+++ libs/_install_libs.bat
@@ -2,12 +2,12 @@
 
 echo Installing GroupDocs libsraries...
 
-call mvn install:install-file -Dpackaging=jar -Dversion=8.0.0   -DgroupId=com.aspose        -DartifactId=aspose-cells    		-Dfile=aspose.cells-8.0.0.jar
-call mvn install:install-file -Dpackaging=jar -Dversion=4.0.1.0 -DgroupId=com.aspose        -DartifactId=aspose-email    		-Dfile=aspose.email-4.0.1.0.jar
+call mvn install:install-file -Dpackaging=jar -Dversion=8.1.1   -DgroupId=com.aspose        -DartifactId=aspose-cells    		-Dfile=aspose.cells-8.1.1.jar
+call mvn install:install-file -Dpackaging=jar -Dversion=4.3.0.0 -DgroupId=com.aspose        -DartifactId=aspose-email    		-Dfile=aspose.email-4.3.0.0.jar
 call mvn install:install-file -Dpackaging=jar -Dversion=2.1.0.0 -DgroupId=com.aspose        -DartifactId=aspose-imaging  		-Dfile=aspose.imaging-2.1.0.0.jar
 call mvn install:install-file -Dpackaging=jar -Dversion=9.1.0   -DgroupId=com.aspose        -DartifactId=aspose-pdf      		-Dfile=aspose.pdf-9.1.0.jar
 call mvn install:install-file -Dpackaging=jar -Dversion=8.4.0   -DgroupId=com.aspose        -DartifactId=aspose-slides   		-Dfile=aspose.slides-8.4.0.jar
-call mvn install:install-file -Dpackaging=jar -Dversion=14.5.0 	-DgroupId=com.aspose        -DartifactId=aspose-words    		-Dfile=aspose.words-14.5.0.jar
+call mvn install:install-file -Dpackaging=jar -Dversion=14.6.0 	-DgroupId=com.aspose        -DartifactId=aspose-words    		-Dfile=aspose.words-14.6.0.jar
 
 call mvn install:install-file -Dpackaging=jar -Dversion=2.3.0	-DgroupId=com.groupdocs		-DartifactId=groupdocs-viewer		-Dfile=viewer-2.3.0-final.jar
 call mvn install:install-file -Dpackaging=jar -Dversion=1.5.0	-DgroupId=com.groupdocs		-DartifactId=groupdocs-annotation	-Dfile=annotation-1.5.0-slim.jar
