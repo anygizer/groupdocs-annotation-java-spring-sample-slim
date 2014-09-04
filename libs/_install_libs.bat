a5e7518ff247c78a6080f19dcafa93a7f1145e4f
aleksey.permyakov <aleksey.permyakov@groupdocs.com>
2014-09-04 11:11:39 +0300

    ANNOTATIONJAVA-368: Update GroupDocs.Viewer and Aspose libraries in GroupDocs.Annotation for Java

--- libs/_install_libs.bat
+++ libs/_install_libs.bat
@@ -2,15 +2,15 @@
 
 echo Installing GroupDocs libsraries...
 
-call mvn install:install-file -Dpackaging=jar -Dversion=8.1.1   -DgroupId=com.aspose        -DartifactId=aspose-cells    		-Dfile=aspose.cells-8.1.1.jar
-call mvn install:install-file -Dpackaging=jar -Dversion=4.3.0.0 -DgroupId=com.aspose        -DartifactId=aspose-email    		-Dfile=aspose.email-4.3.0.0.jar
+call mvn install:install-file -Dpackaging=jar -Dversion=8.2.0   -DgroupId=com.aspose        -DartifactId=aspose-cells    		-Dfile=aspose.cells-8.2.0.jar
+call mvn install:install-file -Dpackaging=jar -Dversion=4.4.0.0 -DgroupId=com.aspose        -DartifactId=aspose-email    		-Dfile=aspose.email-4.4.0.0.jar
 call mvn install:install-file -Dpackaging=jar -Dversion=2.1.0.0 -DgroupId=com.aspose        -DartifactId=aspose-imaging  		-Dfile=aspose.imaging-2.1.0.0.jar
 call mvn install:install-file -Dpackaging=jar -Dversion=9.3.1   -DgroupId=com.aspose        -DartifactId=aspose-pdf      		-Dfile=aspose.pdf-9.3.1.jar
 call mvn install:install-file -Dpackaging=jar -Dversion=14.5.0  -DgroupId=com.aspose        -DartifactId=aspose-slides   		-Dfile=aspose.slides-14.5.0.jar
-call mvn install:install-file -Dpackaging=jar -Dversion=14.6.0 	-DgroupId=com.aspose        -DartifactId=aspose-words    		-Dfile=aspose.words-14.6.0.jar
-call mvn install:install-file -Dpackaging=jar -Dversion=2.1.0   -DgroupId=com.aspose        -DartifactId=aspose-diagram    		-Dfile=aspose.diagram-2.1.0.jar
+call mvn install:install-file -Dpackaging=jar -Dversion=14.7.0 	-DgroupId=com.aspose        -DartifactId=aspose-words    		-Dfile=aspose.words-14.7.0.jar
+call mvn install:install-file -Dpackaging=jar -Dversion=2.2.0   -DgroupId=com.aspose        -DartifactId=aspose-diagram    		-Dfile=aspose.diagram-2.2.0.jar
 
-call mvn install:install-file -Dpackaging=jar -Dversion=2.4.0	-DgroupId=com.groupdocs		-DartifactId=groupdocs-viewer		-Dfile=viewer-2.4.0-final.jar
-call mvn install:install-file -Dpackaging=jar -Dversion=1.6.0	-DgroupId=com.groupdocs		-DartifactId=groupdocs-annotation	-Dfile=annotation-1.6.0-slim.jar
+call mvn install:install-file -Dpackaging=jar -Dversion=2.4.1	-DgroupId=com.groupdocs		-DartifactId=groupdocs-viewer		-Dfile=groupdocs-viewer-2.4.1.jar
+call mvn install:install-file -Dpackaging=jar -Dversion=1.6.0	-DgroupId=com.groupdocs		-DartifactId=groupdocs-annotation	-Dfile=groupdocs-annotation-1.6.0.jar
 
 pause
