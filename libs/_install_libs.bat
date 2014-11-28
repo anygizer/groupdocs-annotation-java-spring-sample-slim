5ca77e759a02863f8d2d5c528c98f2384a5b7833
aleksey.permyakov <aleksey.permyakov@groupdocs.com>
2014-11-28 22:47:16 +0200

    ANNOTATIONJAVA-580: Obfuscation functional should be disabled for stypage xml and json files

--- libs/_install_libs.bat
+++ libs/_install_libs.bat
@@ -2,6 +2,7 @@
 
 echo Installing GroupDocs libsraries...
 
+call mvn install:install-file -Dpackaging=jar -Dversion=6.8.0   -DgroupId=com.aspose        -DartifactId=aspose-tasks    		-Dfile=aspose.tasks-6.8.0.jar
 call mvn install:install-file -Dpackaging=jar -Dversion=8.2.1   -DgroupId=com.aspose        -DartifactId=aspose-cells    		-Dfile=aspose.cells-8.2.1.jar
 call mvn install:install-file -Dpackaging=jar -Dversion=4.5.0.0 -DgroupId=com.aspose        -DartifactId=aspose-email    		-Dfile=aspose.email-4.5.0.0.jar
 call mvn install:install-file -Dpackaging=jar -Dversion=2.1.0.0 -DgroupId=com.aspose        -DartifactId=aspose-imaging  		-Dfile=aspose.imaging-2.1.0.0.jar
@@ -12,6 +13,6 @@ call mvn install:install-file -Dpackaging=jar -Dversion=2.3.0   -DgroupId=com.as
 call mvn install:install-file -Dpackaging=jar -Dversion=4.0     -DgroupId=com.microsoft.sqlserver -DartifactId=sqljdbc4         -Dfile=sqljdbc4.jar
 
 call mvn install:install-file -Dpackaging=jar -Dversion=2.5.0	-DgroupId=com.groupdocs		-DartifactId=groupdocs-viewer		-Dfile=groupdocs-viewer-2.5.0.jar
-call mvn install:install-file -Dpackaging=jar -Dversion=1.7.0	-DgroupId=com.groupdocs		-DartifactId=groupdocs-annotation	-Dfile=groupdocs-annotation-1.7.0.jar
+call mvn install:install-file -Dpackaging=jar -Dversion=1.7.1	-DgroupId=com.groupdocs		-DartifactId=groupdocs-annotation	-Dfile=groupdocs-annotation-1.7.1.jar
 
 pause
